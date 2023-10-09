f477098fde207777ab1ef8b7a7814904900aca44
Joshua Hecker <jhecker2001@gmail.com>
2023-10-09 11:19:41 -0500

    Add project files.

---
+++ band_name_generator/band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator!")
+
+print("What city did you grow up in?")
+city = input()
+
+print("What is the name of one of your pets?")
+pet = input()
+
+print("Your band name could be " + city + " " + pet + ".")
